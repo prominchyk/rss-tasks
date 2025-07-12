62c7da6d76a3c31968ac17e04352215e5d443677
AnnaD <Prominchyk@ukr.net>
2025-07-12 19:43:31 +0300

    fix: change repo to own one

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prominchyk-REACT2025Q3/',
+  base: '/rss-tasks/',
   plugins: [react()],
 });
